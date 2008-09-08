8ad3fb6b67e004e1531701d533c8514bfdfd2ee7
Tirra <tirra@darkamd64.(none)>
2008-09-08 18:03:11 +0400

    added new file task.h from old rep

---
+++ include/eza/amd64/task.h
@@ -0,0 +1,32 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * (c) Copyright 2006,2007,2008 MString Core Team <http://mstring.berlios.de>
+ * (c) Copyright 2008 Michael Tsymbalyuk <mtzaurus@gmail.com>
+ *
+ * include/amd64/task.h: AMD64-specific tasks-related constants.
+ */
+
+#ifndef __ARCH_TASK_H__
+#define __ARCH_TASK_H__ 
+
+typedef enum __task_privilege {
+  TPL_KERNEL = 0,  /* Kernel task - the most serious level. */
+  TPL_USER = 1,    /* User task - the least serious level */
+} task_privelege_t;
+
+#endif
+
