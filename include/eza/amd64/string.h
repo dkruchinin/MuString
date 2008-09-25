3d653a051924ebcdea91797de0c387a64b59418a
Alfeiks Kaanoken (madtirra) <madtirra@jarios.org>
2008-09-25 18:18:19 +0400

    Added memset(), memcpy() amd64 optimized functions removed some old string.h archaics
    chiki

---
+++ include/eza/amd64/string.h
@@ -0,0 +1,37 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * (c) Copyright 2006,2007,2008 MuiString Core Team <http://mstring.jarios.org>
+ * (c) Copyright 2008 MadTirra <madtirra@jarios.org>
+ *
+ * include/eza/amd64/string.h: prototypes and defines for amd64
+ *                            optimized string functions
+ *
+ */
+
+#ifndef __ARCH_STRING_H__
+#define __ARCH_STRING_H__
+
+#include <eza/arch/types.h>
+
+#define ARCH_MEMSET
+extern void *arch_memset(void *dest,int sym,size_t size);
+
+#define ARCH_MEMCPY
+extern void *arch_memcpy(void *dest, const void *src, size_t size);
+
+
+#endif /* __ARCH_STRING_H__ */
