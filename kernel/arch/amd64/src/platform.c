7e36613701b280a14793ad8ce84705f1a440bbd1
Alfeiks Kaanoken (madtirra) <madtirra@jarios.org>
2009-08-03 17:56:40 +0400

    Added Epoch workaround

--- kernel/arch/amd64/src/platform.c
+++ kernel/arch/amd64/src/platform.c
@@ -15,7 +15,7 @@
  * 02111-1307, USA.
  *
  * (c) Copyright 2006,2007,2008 MString Core Team <http://mstring.jarios.org>
- * (c) Copyright 2008 Tirra <tirra.newly@gmail.com>
+ * (c) Copyright 2008 Tirra <madtirra@jarios.org>
  * (c) Copyright 2008 Dmitry Gromada <gromada@jarios.org>
  *
  * mstring/amd64/platform.c: specific platform initialization.
@@ -99,10 +99,39 @@ void arch_ap_specific_init(void)
 
 #endif
 
+static const int mon_days[12] = {
+  31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
+static ulong_t __init_tsepoch(void)
+{
+  ulong_t r=0; /* -1900 */
+  int i;
+
+  r=s_epoch.tm_sec + s_epoch.tm_min * 60 + s_epoch.tm_hour*3600 +
+    (s_epoch.tm_mday-1)*86400;
+
+  /* month */
+  for(i=0;i<s_epoch.tm_mon;i++) {
+    r+=mon_days[i]*86400;
+    if((i==1) && (s_epoch.tm_year % 4 ==2 ))
+      r+=86400;
+  }
+
+  /* year */
+  for(i=0;i<s_epoch.tm_year;i++) {
+    r+=365*86400;
+    if(i % 4 == 2) r +=86400;
+  }
+
+  return r;
+}
+
 void arch_initialize_swks(void)
 {
  /* According to the x86 programming manual, we must reserve
   * eight bits after the last available I/O port in all IOPMs.
-  */ 
+  */
   swks.ioports_available=TSS_IOPORTS_LIMIT;
+  swks.secs_since_epoch=__init_tsepoch();
 }
--- kernel/arch/amd64/src/time.c
+++ kernel/arch/amd64/src/time.c
@@ -1,7 +1,108 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * (c) Copyright 2006,2007,2008,2009 MString Core Team <http://mstring.jarios.org>
+ * (c) Copyright 2008,2009 Tirra <madtirra@jarios.org>
+ *
+ * mstring/amd64/time.c: specific time operations
+ *
+ */
+
 #include <arch/types.h>
+#include <mstring/types.h>
 #include <mstring/time.h>
 #include <mstring/swks.h>
 
+/* defined values for BIOS time */
+/* RTC io ports */
+#define RTC_ADDRESS 0x70
+#define RTC_DATA 0x71
+/* RTC registers */
+
+/* Current time and date */
+#define RTC_SECONDS 0x0
+#define RTC_MINUTES 0x2
+#define RTC_HOURS 0x4
+#define RTC_DAY_OF_WEEK 0x6
+#define RTC_DAY_OF_MONTH 0x7
+#define RTC_MONTH 0x8
+#define RTC_YEAR 0x9
+
+/* Alarm control */
+#define RTC_ALARM_SECONDS 0x1
+#define RTC_ALARM_MINUTES 0x3
+#define RTC_ALARM_HOURS 0x5
+
+/* macros for calc */
+#define BCD2BIN(x) ((x) & 0x0f) + ((x) >> 4) * 10
+#define BIN2BCD(x) (((x) / 10) << 4) + (x) % 10
+
+/* work around with BIOS clock */
+static uint8_t rtc_read_register(uint8_t reg)
+{
+  outb(RTC_ADDRESS, reg);
+  return inb(RTC_DATA);
+}
+
+#if 0
+static void rtc_write_register(uint8_t reg, uint8_t val)
+{
+  outb(RTC_ADDRESS, reg);
+  outb(RTC_DATA, val);
+}
+#endif
+
+static void get_rtc_time(struct tm *time)
+{
+  /* Overflow of seconds or minutes may happen,
+   * so we check it to return consistent values.
+   */
+  for (;;) {
+    time->tm_sec = rtc_read_register(RTC_SECONDS);
+    time->tm_min = rtc_read_register(RTC_MINUTES);
+    time->tm_hour = rtc_read_register(RTC_HOURS);
+    if ((time->tm_min == rtc_read_register(RTC_MINUTES)) &&
+        (time->tm_hour == rtc_read_register(RTC_HOURS)))
+      break;
+  }
+  time->tm_mday = rtc_read_register(RTC_DAY_OF_MONTH);
+  time->tm_mon = rtc_read_register(RTC_MONTH);
+  time->tm_year = rtc_read_register(RTC_YEAR);
+
+  time->tm_sec = BCD2BIN(time->tm_sec);
+  time->tm_min = BCD2BIN(time->tm_min);
+  time->tm_hour = BCD2BIN(time->tm_hour);
+  time->tm_mday = BCD2BIN(time->tm_mday);
+  time->tm_mon = BCD2BIN(time->tm_mon);
+  time->tm_year = BCD2BIN(time->tm_year);
+  time->tm_wday = BCD2BIN(time->tm_wday);
+
+  if (time->tm_year <= 69)
+    time->tm_year += 100;
+  time->tm_mon--;
+
+  time->tm_wday = 0;
+  time->tm_yday = 0;
+  time->tm_isdst = 0;
+  return;
+}
+
+/* TODO: add write function, it may be called when shutdown going */
+
 void arch_setup_time(void)
 {
+  get_rtc_time(&s_epoch);
 }
