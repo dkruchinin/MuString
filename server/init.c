86ce27c3f42e25ff4c75303129f81f0f8ce8897e
Alfeiks Kaanoken (madtirra) <madtirra@jarios.org>
2011-06-22 22:55:12 +0300

    Code clean up. Some code was moved.

--- server/init.c
+++ server/init.c
@@ -82,156 +82,12 @@ out:
   return ret;
 }
 
-struct bin_map {
-  ulong_t bin_addr;
-  ulong_t virt_addr;
-  ulong_t size;
-  uint32_t flags;
-  uint32_t type;
-  struct bin_map *next;
-  struct bin_map *prev;
-};
-
-/* memalloc / memfree */
-
-static struct bin_map *__add_bm_region(struct bin_map *cur,
-                                       struct bin_map *new)
-{
-  cur->next = new;
-  new->next = NULL;
-  new->prev = cur;
-
-  return new;
-}
-
-static void __destroy_bin_map(struct bin_map *root)
-{
-  struct bin_map *cur;
-
-  while(root!=NULL) {
-    cur = root;
-    root = cur->next;
-    memfree(cur);
-  }
-
-  return;
-}
-
-static struct bin_map *__get_elf_map(task_t *task, init_server_t *srv)
-{
-  char *addr;
-  elf_head_t ehead;
-  elf_pr_t epr;
-  elf_sh_t esh;
-  struct bin_map *root, *cur = NULL, *new = NULL;
-  int i;
-
-  addr = pframe_id_to_virt((srv->addr)>>PAGE_WIDTH);
-
-  /* read elf headers */
-  memcpy(&ehead, addr, sizeof(elf_head_t));
-
-  /* FIXME: add ELF header check */
-
-  root = memalloc(sizeof(struct bin_map));
-  if(!root) return NULL;
-  else {
-    root->next = NULL;
-    root->prev = NULL;
-  }
-
-#if 0
-  /* printf elf header info */
-  kprintf("ELF header(%4s): %d type, %d mach, %d version\n", ehead.e_ident,
-          ehead.e_type, ehead.e_machine, ehead.e_version);
-  kprintf("Entry: %p,Image off: %p,sect off:%p\n", ehead.e_entry, ehead.e_phoff,
-          ehead.e_shoff);
-#endif
-
-  for(i=0; i<ehead.e_shnum; i++) {
-    memcpy(&esh, addr + ehead.e_shoff +
-           i*(ehead.e_shentsize), sizeof(esh));
-
-    if((esh.sh_size != 0) && ((esh.sh_flags & ESH_ALLOC) &&
-                              ((esh.sh_type == SHT_PROGBITS) ||
-                               (esh.sh_type == SHT_NOBITS)))) {
-
-      if(!cur) {
-        root->virt_addr = esh.sh_addr;
-        root->bin_addr = srv->addr + esh.sh_offset;
-        root->size = esh.sh_size;
-        root->type = esh.sh_type;
-        root->flags = esh.sh_flags;
-
-        cur = root;
-      } else {
-        if(!(new = memalloc(sizeof(struct bin_map))))
-          return NULL;
-
-        new->virt_addr = esh.sh_addr;
-        new->bin_addr = srv->addr + esh.sh_offset;
-        new->size = esh.sh_size;
-        new->type = esh.sh_type;
-        new->flags = esh.sh_flags;
-
-        cur = __add_bm_region(cur, new);
-      }
-    }
-
-  }
-
-  /* split areas */
-  cur = root;
-  while(cur->next != NULL) {
-    /* might be splitted */
-    if((PAGE_ALIGN_DOWN(cur->virt_addr + cur->size) ==
-        PAGE_ALIGN_DOWN(cur->next->virt_addr)) && (cur->type == cur->next->type)) {
-
-      cur->size += cur->next->size;
-      new = cur->next;
-      cur->next = cur->next->next;
-      if(cur->next)
-        cur->next->prev = cur;
-      memfree(new);
-    } else cur = cur->next;
-  }
-
-#if 0
-  cur = root;
-  while(cur) {
-    kprintf("Got section:\n");
-    kprintf("\tType: ");
-    if(cur->type == SHT_NOBITS) kprintf("Absent in image\n");
-    else kprintf("Exist in image\n");
-    kprintf("\tVirt addr: %p, size %ld, img_addr %p\n", cur->virt_addr,
-            cur->size, cur->bin_addr);
-
-    cur = cur->next;
-  }
-#endif
-
-  return root;
-}
-
-static ulong_t __get_elf_entry(task_t *task, init_server_t *srv)
-{
-  char *addr;
-  elf_head_t ehead;
-
-  addr = pframe_id_to_virt((srv->addr)>>PAGE_WIDTH);
-
-  /* read elf headers */
-  memcpy(&ehead, addr, sizeof(elf_head_t));
-
-  return ehead.e_entry;
-}
-
 static void __create_task_mm(task_t *task, int num, init_server_t *srv)
 {
-  struct bin_map *emap = __get_elf_map(task,srv);
+  struct bin_map *emap = get_elf_map(task,srv);
   per_task_data_t *ptd;
   vmm_t *vmm = task->task_mm;
-  ulong_t entry = __get_elf_entry(task,srv);
+  ulong_t entry = get_elf_entry(task,srv);
   struct bin_map *cur = emap;
   ulong_t sseek = 0, psize;
   void *sbss;
@@ -356,6 +212,9 @@ static void __create_task_mm(task_t *task, int num, init_server_t *srv)
   if (r < 0)
     panic("Server [#%d]: Failed to set task's stack(%p). (ERR = %d)", num,
           ustack_top, r);
+
+  destroy_bin_map(emap);
+
   return;
 }
 
